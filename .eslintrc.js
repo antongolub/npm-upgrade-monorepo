b1ce2cd778405c53319a0717dc6fcf418b446857
Anton Golub <mailbox@antongolub.ru>
2021-02-24 20:08:11 +0300

    fix(package): up deps, fix vuls

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
     extends: [
         'eslint-config-qiwi',
         'prettier',
-        'prettier/@typescript-eslint',
     ],
     rules: {
         'sonarjs/no-duplicate-string': 'off',
